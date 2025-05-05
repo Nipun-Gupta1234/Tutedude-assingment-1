b9fe834c111f2dac42b7ceeb36bae6c8edde4eea
Nipun-Gupta1234 <nipungupta735@gmail.com>
2025-05-05 16:59:29 +0530

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -1,6 +1,8 @@
+#Entering the two numbers
 a=float(input("Enter the first number: "))
 b=float(input("Enter the second number: "))
 
+#Performing the desired operations
 print("Addition:", a+b)
 print("Subtraction:", a-b)
 print("Multiplication:", a*b)
